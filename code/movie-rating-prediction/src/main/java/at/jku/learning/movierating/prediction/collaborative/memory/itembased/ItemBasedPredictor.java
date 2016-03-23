3307c77762c6ec25e251581b6becb0cc12fc8874
Markus Hiesmair <markus.hiesmair@yahoo.de>
2016-03-23 11:44:16 +0100

    item-based - do more work at first than in each prediction

--- code/movie-rating-prediction/src/main/java/at/jku/learning/movierating/prediction/collaborative/memory/itembased/ItemBasedPredictor.java
+++ code/movie-rating-prediction/src/main/java/at/jku/learning/movierating/prediction/collaborative/memory/itembased/ItemBasedPredictor.java
@@ -2,6 +2,7 @@ package at.jku.learning.movierating.prediction.collaborative.memory.itembased;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,7 +18,7 @@ public class ItemBasedPredictor implements Predictor {
 	private List<Rating> trainingSet;
 	private Map<Long, Double> userRatingAverages;
 	private Map<Map.Entry<Long, Long>, Double> itemSimilarityCache;
-	private Map<Long, List<Rating>> trainingSetByItemId;
+	private Map<Long, Map<Long, Rating>> trainingSetByItemAndUser;
 	private Map<Long, List<Rating>> trainingSetByUserId;
 	private Integer highestSimilarityItemOffset; // in slides = k
 
@@ -26,8 +27,10 @@ public class ItemBasedPredictor implements Predictor {
 		this.trainingSet = trainingSet;
 		this.userRatingAverages = new HashMap<>();
 		this.itemSimilarityCache = new HashMap<>();
-		this.trainingSetByItemId = this.trainingSet.stream().collect(Collectors.groupingBy(i -> i.getMovieId()));
+		this.trainingSetByItemAndUser = this.trainingSet.stream()
+				.collect(Collectors.groupingBy(i -> i.getMovieId(), Collectors.toMap(i -> i.getUserId(), i -> i)));
 		this.trainingSetByUserId = this.trainingSet.stream().collect(Collectors.groupingBy(i -> i.getUserId()));
+		
 		this.highestSimilarityItemOffset = highestSimilarityItemOffset;
 
 		init();
@@ -61,12 +64,12 @@ public class ItemBasedPredictor implements Predictor {
 	private Map<Long, Double> getMostSimilarItems(Long unknownItemId, Long userId) {
 		List<Rating> userRatings = trainingSetByUserId.get(userId);
 		
-		List<Rating> ratings1 = trainingSetByItemId.get(unknownItemId);
+		Map<Long, Rating> ratings1 = trainingSetByItemAndUser.get(unknownItemId);
 		
 		List<Map.Entry<Long, Double>> similarItems = new ArrayList<>();
-		for (Long item : trainingSetByItemId.keySet()) {
+		for (Long item : trainingSetByItemAndUser.keySet()) {
 			if (!item.equals(unknownItemId) && userRatings.stream().anyMatch(r -> r.getMovieId().equals(item))) {
-				List<Rating> ratings2 = trainingSetByItemId.get(item);
+				Map<Long, Rating> ratings2 = trainingSetByItemAndUser.get(item);
 				Double similarity = calculateSimilarity(unknownItemId, ratings1, item, ratings2);
 				
 				similarItems.add(new AbstractMap.SimpleEntry<>(item, similarity));
@@ -82,26 +85,26 @@ public class ItemBasedPredictor implements Predictor {
 	/**
 	 * formula slide 26
 	 */
-	protected Double calculateSimilarity(Long itemId1, List<Rating> ratings1, Long itemId2, List<Rating> ratings2) {
+	protected Double calculateSimilarity(Long itemId1, Map<Long, Rating> ratings1, Long itemId2, Map<Long, Rating> ratings2) {
 		Double cachedSimilarity = itemSimilarityCache.get(new AbstractMap.SimpleEntry<>(itemId1, itemId2));
 		if (cachedSimilarity != null) {
 			return cachedSimilarity;
 		}
 		
-		Set<Long> userBase = getUserBase(ratings1, ratings2);
+		Set<Long> userBase = getUserBase(ratings1.values(), ratings2.values());
 		
 		Double a = 0.0;
 		Double b1 = 0.0;
 		Double b2 = 0.0;
 		
 		for (Long user : userBase) {
-			Double userBaseAverage = calculateUserBaseAverage(user);
-			Integer rating1 = ratings1.stream().filter(r -> r.getUserId().equals(user)).findFirst().get().getRating();
-			Integer rating2 = ratings2.stream().filter(r -> r.getUserId().equals(user)).findFirst().get().getRating();
+			Double userRatingAverage = getUserRatingAverage(user);
+			Integer rating1 = ratings1.get(user).getRating();
+			Integer rating2 = ratings2.get(user).getRating();
 			
-			a += (rating1 - userBaseAverage) * (rating2 - userBaseAverage);
-			b1 += (rating1 - userBaseAverage) * (rating1 - userBaseAverage);
-			b2 += (rating2 - userBaseAverage) * (rating2 - userBaseAverage);
+			a += (rating1 - userRatingAverage) * (rating2 - userRatingAverage);
+			b1 += (rating1 - userRatingAverage) * (rating1 - userRatingAverage);
+			b2 += (rating2 - userRatingAverage) * (rating2 - userRatingAverage);
 		}
 
 		Double similarity = a / (Math.sqrt(b1) * Math.sqrt(b2));
@@ -112,7 +115,7 @@ public class ItemBasedPredictor implements Predictor {
 		return similarity;
 	}
 
-	private Double calculateUserBaseAverage(Long userId) {
+	private Double getUserRatingAverage(Long userId) {
 		Double userRatingAverage = userRatingAverages.get(userId);
 		if (userRatingAverage == null) {
 			userRatingAverage = trainingSetByUserId.get(userId).stream().mapToInt(r -> r.getRating()).average().getAsDouble();
@@ -127,7 +130,7 @@ public class ItemBasedPredictor implements Predictor {
 	 * 
 	 * in slides = U
 	 */
-	private Set<Long> getUserBase(List<Rating> ratings1, List<Rating> ratings2) {
+	private Set<Long> getUserBase(Collection<Rating> ratings1, Collection<Rating> ratings2) {
 		Set<Long> userBase = new HashSet<>();
 		userBase.addAll(ratings1.stream().map(m -> m.getUserId()).collect(Collectors.toList()));
 		userBase.retainAll(ratings2.stream().map(m -> m.getUserId()).collect(Collectors.toList()));
