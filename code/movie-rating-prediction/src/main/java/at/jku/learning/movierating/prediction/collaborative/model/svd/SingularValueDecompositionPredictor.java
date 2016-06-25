0be8140eb45409ced6f59a2945b6bac91fd90858
RSulzer <sulzer.r@gmail.com>
2016-06-25 13:42:22 +0200

    Changed baseline and rsme check for SVD

--- code/movie-rating-prediction/src/main/java/at/jku/learning/movierating/prediction/collaborative/model/svd/SingularValueDecompositionPredictor.java
+++ code/movie-rating-prediction/src/main/java/at/jku/learning/movierating/prediction/collaborative/model/svd/SingularValueDecompositionPredictor.java
@@ -1,5 +1,6 @@
 package at.jku.learning.movierating.prediction.collaborative.model.svd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,12 +20,13 @@ import at.jku.learning.movierating.prediction.Predictor;
 public class SingularValueDecompositionPredictor implements Predictor {
   
   private List<Rating> trainingSet;
+  private List<Rating> testSet = new ArrayList<Rating>();
   private Set<FeatureSVD> featureSet;
   private Map<Long, Double> movieMeanMap;
+  //private Map<Long, Double> itemOffsetMap;  
   private Map<Long, Double> userOffsetMap;
   private final int cycles;
   private final int featureLimit;
-  private final int meanMultiplier;
   private final Double learningRate;
   private final Double regularizationTerm;
   private Double globalMovieRatingMean = 0.0;
@@ -35,7 +37,6 @@ public class SingularValueDecompositionPredictor implements Predictor {
     this.learningRate = ParameterSVD.INSTANCE.learningRate;
     this.featureLimit = ParameterSVD.INSTANCE.featureLimit;
     this.regularizationTerm = ParameterSVD.INSTANCE.regularizationTerm;
-    this.meanMultiplier = ParameterSVD.INSTANCE.meanMultiplier;
   }
   
   public SingularValueDecompositionPredictor(int cycles) {
@@ -43,17 +44,14 @@ public class SingularValueDecompositionPredictor implements Predictor {
     this.learningRate = ParameterSVD.INSTANCE.learningRate;
     this.featureLimit = ParameterSVD.INSTANCE.featureLimit;
     this.regularizationTerm = ParameterSVD.INSTANCE.regularizationTerm;
-    this.meanMultiplier = ParameterSVD.INSTANCE.meanMultiplier;
   } 
   
   public SingularValueDecompositionPredictor(int cycles, 
                                              Double learningRate, 
-                                             int featureLimit, 
-                                             int meanMultiplier) {
+                                             int featureLimit) {
     this.cycles = cycles;
     this.learningRate = learningRate;
     this.featureLimit = featureLimit;
-    this.meanMultiplier = meanMultiplier;
     this.regularizationTerm = ParameterSVD.INSTANCE.regularizationTerm;
      
   }
@@ -78,18 +76,43 @@ public class SingularValueDecompositionPredictor implements Predictor {
     train();
   }
   
+  public void setTestSet(List<Rating> testSet) {
+    this.testSet = testSet;
+  }
+  
+  public Double calculateRMSE(){
+    
+    Double rmseError = 0.0;
+    Double squaredError = 0.0;
+    for(Rating rating : this.testSet) {
+      //Calculate the error
+      Double error = rating.getRating()- predictRating(rating.getUserId(),rating.getMovieId());
+      squaredError += Math.pow(error, 2);
+    }
+    
+    rmseError = Math.sqrt(squaredError / testSet.size());
+    
+    return rmseError;
+    
+  }
+  
   private void train() {
     FeatureSVD feature;
     featureSet = new HashSet<FeatureSVD>();
     computePreTrain();
     
+    Double[] rmseErrorForEpoch = new Double[this.cycles];
+    
+    
     for (int featureRank=1; featureRank <= this.featureLimit; ++featureRank) {
       feature = new FeatureSVD(featureRank); //Create new Feature
       featureSet.add(feature);
+      Double fastLearningRate = this.learningRate*2;
       
       //Eventually check if RMSE changes just a little between two epochs and then skip instead of fixed cycles
       //Alternatively check if the sum of changes of feature values are rather small
-      for (int epoch=1; epoch <= this.cycles; ++epoch) {
+      for (int epoch=0; epoch < this.cycles; ++epoch) {
+        rmseErrorForEpoch[epoch] = 0.0;
         for(Rating rating : this.trainingSet) {
           // set feature
           
@@ -98,15 +121,33 @@ public class SingularValueDecompositionPredictor implements Predictor {
           
           //Update the features
           Double userFeature = feature.getUserFeature(rating.getUserId());
-          userFeature += this.learningRate*(error*feature.getItemFeature(rating.getMovieId()) 
+          userFeature += fastLearningRate*(error*feature.getItemFeature(rating.getMovieId()) 
               - (regularizationTerm*feature.getUserFeature(rating.getUserId())));
           
           Double itemFeature = feature.getItemFeature(rating.getMovieId());
-          itemFeature += this.learningRate*(error*feature.getUserFeature(rating.getUserId()) 
+          itemFeature += fastLearningRate*(error*feature.getUserFeature(rating.getUserId()) 
               - regularizationTerm*feature.getItemFeature(rating.getMovieId()));
           
           feature.setUserFeature(rating.getUserId(), userFeature);
           feature.setItemFeature(rating.getMovieId(), itemFeature);      
+        }
+        
+        rmseErrorForEpoch[epoch] += calculateRMSE();
+        
+        
+        if(this.learningRate < fastLearningRate){
+          fastLearningRate -= this.learningRate/20;
+        } else {
+          fastLearningRate = this.learningRate;
+        }
+        
+        if(epoch > 0 && !testSet.isEmpty()) {
+          //System.out.println("Feature:" + featureRank + ", Epoch:" + epoch + ", Error:" + rmseErrorForEpoch[epoch] + ", LR:" + fastLearningRate);
+          if( rmseErrorForEpoch[epoch] > rmseErrorForEpoch[epoch-1]) {
+            //System.out.println("Best results with epoch:" + epoch +  
+            //    ", Feature:" + featureRank + ", Error:" + rmseErrorForEpoch[epoch]);
+            break;
+          }
         }
       }
     }
@@ -132,6 +173,7 @@ public class SingularValueDecompositionPredictor implements Predictor {
     Long countMovie = 0L;
     Long countUser = 0L;
     //Double globalMovieRatingMean = 0.0;
+    Double sumItemOffset = 0.0;
     Double flattenedMean = 0.0;
     Double userOffset = 0.0;
     int k = ParameterSVD.INSTANCE.meanMultiplier;
@@ -154,10 +196,12 @@ public class SingularValueDecompositionPredictor implements Predictor {
     for (Map.Entry<Long, List<Rating>> userRatings : trainingSetByUserId.entrySet()) {
       userId = userRatings.getKey();
       sumUser = trainingSetByUserId.get(userId).stream().mapToLong(r -> r.getRating()).sum();
+      sumItemOffset = trainingSetByUserId.get(userId).stream().mapToDouble(r -> movieMeanMap.get(r.getMovieId())).sum();
       countUser = trainingSetByUserId.get(userId).stream().mapToLong(r -> r.getRating()).count();
       
-      flattenedMean = (globalMovieRatingMean*k + sumUser)/(k + countUser);
-      userOffset = globalMovieRatingMean - flattenedMean;
+      userOffset = (sumUser - sumItemOffset)/countUser;
+      //flattenedMean = (globalMovieRatingMean*k + sumUser)/(k + countUser);
+      //userOffset = globalMovieRatingMean - flattenedMean;
       userOffsetMap.put(userId, userOffset);
     }
     
@@ -165,16 +209,16 @@ public class SingularValueDecompositionPredictor implements Predictor {
   
   @Override
 	public String toString() {
-		return "SVD cycles=" + cycles;
+		return "SVD cycles=" + cycles + ", features=" + featureLimit + "\n";
 	}
   
   private enum ParameterSVD {
     INSTANCE;
-    private final int cycles = 120; 
+    private final int cycles = 160; 
     private final Double learningRate = 0.001;
     private final int featureLimit = 40; 
     private final Double regularizationTerm = 0.02;
-    private final int meanMultiplier = 10; //Seems to work well
+    private final int meanMultiplier = 8; 
   }
 
 }
